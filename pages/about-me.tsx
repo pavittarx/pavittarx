f9ff3399ca2501eff14a06bbfe9c3e501a4e8e78
pavittarx <29155477+pavittarx@users.noreply.github.com>
2021-08-16 16:08:11 +0530

    about page added

---
+++ pages/about-me.tsx
@@ -0,0 +1,12 @@
+import { useRouter } from "next/router";
+import Layout from "@/_layouts/main";
+
+const AboutPage = () => {
+  return (
+    <Layout title="About | @pavittarx">
+      <div />
+    </Layout>
+  );
+};
+
+export default AboutPage;
