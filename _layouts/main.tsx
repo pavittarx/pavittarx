daa55b095855508f8a69dc579d05e88b195a92d8
pavittarx <29155477+pavittarx@users.noreply.github.com>
2025-10-31 12:43:05 +0000

    fix: update prop types and improve title formatting in layout

--- _layouts/main.tsx
+++ _layouts/main.tsx
@@ -1,18 +1,17 @@
 import Head from "next/head";
 import Header from "@/components/Header";
-import { string } from "prop-types";
-import { ReactChild } from "react";
+import { ReactNode } from "react";
 
 type LayoutProps = {
   title: string;
-  children: ReactChild;
+  children: ReactNode;
 };
 
 const MainLayout = ({ title, children }: LayoutProps) => {
   return (
     <>
       <Head>
-        <title> {title ? title : "pavittarx"} </title>
+        <title>{title ? `${title}` : "pavittarx"}</title>
       </Head>
       <Header />
       <main className="main-ctr">{children}</main>
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -15,7 +15,7 @@ function MyApp({ Component, pageProps }: AppProps) {
       <link
         rel="preconnect"
         href="https://fonts.gstatic.com"
-        crossOrigin="true"
+        crossOrigin="anonymous"
       />
       <link
         href="https://fonts.googleapis.com/css2?family=IBM+Plex+Sans:ital,wght@0,400;0,500;0,600;1,400;1,500;1,600&display=swap"
